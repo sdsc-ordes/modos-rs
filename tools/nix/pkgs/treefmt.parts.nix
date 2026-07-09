689b52806ed93ad31e76052e6143453ed6138afe
almutlue <almut.lue@gmail.com>
2026-07-09 16:24:29 +0200

    refactor: add treefmt flake parts module

--- tools/nix/pkgs/treefmt.parts.nix
+++ tools/nix/pkgs/treefmt.parts.nix
@@ -1,16 +1,76 @@
 { inputs, ... }:
 {
+  imports = [
+    inputs.treefmt-nix.flakeModule
+  ];
+
   perSystem =
-    { pkgs, ... }:
+    { config, pkgs, ... }:
     let
-      treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-      treefmt = treefmtEval.config.build.wrapper;
+      treefmt = config.treefmt.build.wrapper;
     in
     {
-      modos.packages.global = {
+      # Define formatter for `nix fmt`.
+      formatter = treefmt;
+
+      # Define formatter as global output.
+      packages = {
         inherit treefmt;
       };
 
-      formatter = treefmt;
+      treefmt = {
+        inherit pkgs;
+        # Used to find the project root
+        # For worktrees we need either `.git` or a file.
+        projectRootFile = "CHANGELOG.md";
+
+        settings.global = {
+          excludes = [
+            "external/**/*"
+            "**/vendor/**/*"
+          ];
+        };
+
+        # Enable the following formatters.
+        programs.gofmt.enable = true;
+        programs.goimports.enable = true;
+        programs.golines.enable = true;
+
+        # Markdown, JSON, YAML, etc.
+        programs.prettier.enable = true;
+        settings.formatter.prettier.excludes = [
+          "*/api/openapi*" # these are symlinks, which prettier cannot deal with
+          ".golangci.yaml" # this is a symlink, which prettier cannot deal with
+          ".yamllint.yaml" # this is a symlink, which prettier cannot deal with
+        ];
+
+        programs.ruff-format.enable = true;
+
+        # Shellscripts (which we should not have!)
+        programs.shfmt = {
+          enable = true;
+          indent_size = 4;
+        };
+        programs.shellcheck = {
+          enable = true;
+        };
+        settings.formatter.shellcheck = {
+          options = [
+            "-e"
+            "SC1091"
+          ];
+        };
+
+        # Nix.
+        programs.deadnix.enable = false;
+        programs.statix.enable = false;
+        programs.nixfmt.enable = true;
+
+        # Lua.
+        programs.stylua.enable = true;
+
+        # Typos. TODO: Make this work only for markdown, its destructive in other formats.
+        # programs.typos.enable = true;
+      };
     };
 }
