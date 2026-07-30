2eb2106e698aa066161cf5cc6d27cb4e856b4b6b
Gabriel Nützi <gabriel.nuetzi@sdsc.ethz.ch>
2026-07-30 11:10:04 +0200

    fix: delete dir

--- tools/nix/lib/service/authentik/service.nix
+++ tools/nix/lib/service/authentik/service.nix
@@ -163,7 +163,7 @@ let
       if [ ! -L "$dataDir/temp" ]; then
         tmpDir=$(mktemp -d)
         mkdir -p "$tmpDir"
-        chmod 755 "$tmpDir"
+        chmod -R 755 "$tmpDir"
         ln -s "$tmpDir" "$dataDir/temp"
       fi
       export TMPDIR="$tmpDir"
